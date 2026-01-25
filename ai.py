c200af86d574e81a5728688fa824a666b4c17490
isaias <isaiasgarraeluta@gmail.com>
2026-01-25 01:23:20 -0300

    **Resumo das mudanças**
    - Refatoração da função `getCommitMessageWithAI` para melhorar a legibilidade:
      - Unificação da criação do objeto `ChatGroq` em uma única linha.
      - Reestruturação da lista `memory` com formatação multiline e identação clara.
    - Inclusão da chamada ao modelo (`llm.invoke(memory)`) e retorno do conteúdo da resposta (`return response.content`).
    - Ajustes de espaçamento e remoção de linhas em branco desnecessárias.

---
+++ ai.py
@@ -0,0 +1,16 @@
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage, SystemMessage
+
+
+def getCommitMessageWithAI(prompt):
+    llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0, max_retries=2)
+
+    memory = [
+        SystemMessage(
+            content="você é um especialista em git que compara diff de arquivos e escreve mensagens apropriadas para commits"
+        ),
+        HumanMessage(content=prompt),
+    ]
+    response = llm.invoke(memory)
+
+    return response.content
--- sync.py
+++ sync.py
@@ -1,19 +1,47 @@
 import subprocess
+import re
+import ai
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def sync_on_github():
     print("sync on github...")
 
-    result = subprocess.run(["git", "status"], capture_output=True, text=True)
+    result = subprocess.run(["git", "status", "-sb"], capture_output=True, text=True)
 
-    if "add" in result.stdout:
-        message = get_commit_description()
-
+    if "M" in result.stdout or "??" in result.stdout:
+        message = get_commit_description(result.stdout)
+        
         subprocess.run(["git", "add", "."])
         subprocess.run(["git", "commit", "-m", message])
         subprocess.run(["git", "push"])
 
-def get_commit_description():
-    return "automatic adition"
+
+def get_commit_description(output: str):
+    updateds = re.findall(r"(?<=M ).+", output)
+    createds = re.findall(r"(?<=\?\?).+", output)
+
+    changes="crie uma mensagem com o resumo das mudanças ocorridas:\n"
+    for update in updateds:
+        changes+="diff de arquivo existente:\n"+get_diff(update)
+
+    for created in createds:
+        changes+="diff de arquivo adicionado:\n"+get_diff(created)
+        
+    message = ai.getCommitMessageWithAI(changes)
+    
+    return message
+
+
+
+       
+       
+
+def get_diff(path: str):
+    diff_exec = subprocess.run(["git","--no-pager" ,"diff", path],capture_output=True, text=True)
+    
+    return diff_exec.stdout
+
 
 sync_on_github()
