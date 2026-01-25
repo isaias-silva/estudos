ae4b4eb2ac622c1e0c914df0608fb4aa097aff3d
isaias <isaiasgarraeluta@gmail.com>
2026-01-25 00:27:31 -0300

    "automatic adition"

--- sync.py
+++ sync.py
@@ -5,7 +5,11 @@ def sync_on_github():
     print("sync on github...")
     
     result = subprocess.run(["git", "status"], capture_output=True, text=True)
-
+    
+    if "add" in result.stdout:
+        subprocess.run(["git", "add", "."]);
+        subprocess.run(["git","commit", "-m", "\"automatic adition\""])
+        subprocess.run(["git", "push"])
         
        
 
