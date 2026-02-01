23399e8ecb5d3fe28636f01b96527ebf286a0c3c
isaias <isaiasgarraeluta@gmail.com>
2026-01-31 22:09:57 -0300

    refactor: ajustar prompt da IA para incluir foco em commit semântico

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Estudos gerais
+
+conjunto de markdowns utilizados em meus estudos de tecnologia com obsidian.
+
+### atualizações:
+
+Java ██████░░░░░░ 60%
+
+POO (Orientação a Objetos) █████████░░░ 90%
+
+ElasticSearch █░░░░░░░░░░░ 10%
+
+Rust ░░░░░░░░░░░░ 5%
+
+Python █░░░░░░░░░░░ 10%
+
+
+#### resumo de hoje:
+
+finalização do estudo de programação concorrente com java e virtual threads.
--- scripts/ai.py
+++ scripts/ai.py
@@ -7,7 +7,7 @@ def getCommitMessageWithAI(prompt):
 
     memory = [
         SystemMessage(
-            content="você é um especialista em git que compara diff de arquivos e escreve mensagens apropriadas para commits, você retorna apenas a mensagem de commit em português baseado nas mudanças, sem nada adicional."
+            content="você é um especialista em git que compara diff de arquivos e escreve mensagens apropriadas para commits, você retorna apenas a mensagem de commit em português baseado nas mudanças e com foco em commit semântico, sem nada adicional."
         ),
         HumanMessage(content=prompt),
     ]
